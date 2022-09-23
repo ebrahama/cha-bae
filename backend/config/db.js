65561e75ab2e89949ca00df30718c231ca97c174
ibrahama <ibrahama2003@gmail.com>
2022-09-23 21:12:29 +0200

    n

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+let MONGO_URI = "mongodb+srv://ibra:moomoo@cluster0.uzmgtsk.mongodb.net/?retryWrites=true&w=majority"
 const condb = async()=>{
 
     try{
-      const con = await mongoose.connect(process.env.MONGO_URI, {
+      const con = await mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
--- backend/config/genretoken.js
+++ backend/config/genretoken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
-
+let JWT_SECRET = "ibr"
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, JWT_SECRET, {
     expiresIn: "30d",
   });
 }; 
